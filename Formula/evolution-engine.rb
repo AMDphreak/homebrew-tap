96e103d9405ade6df350bcca3785efe709664742
Ryan Johnson <ryan.johnson.code@gmail.com>
2026-03-23 04:35:57 -0500

    evolution-engine v1.3.1

--- Formula/evolution-engine.rb
+++ Formula/evolution-engine.rb
@@ -1,9 +1,9 @@
 class EvolutionEngine < Formula
   desc "Core engine for the Evolution Adaptation Ecosystem"
   homepage "https://github.com/AMDphreak/evolution-engine"
-  url "https://github.com/AMDphreak/evolution-engine/releases/download/v1.3.0/evolution-engine-macos-latest"
+  url "https://github.com/AMDphreak/evolution-engine/releases/download/v1.3.1/evolution-engine-macos-latest"
   sha256 "7002f2538b8f562490172bd3227d23a25fa14903d825f36fc464d0b9134a2ea4"
-  version "1.3.0"
+  version "1.3.1"
 
   def install
     bin.install "evolution-engine-macos-latest" => "evolution-engine"
